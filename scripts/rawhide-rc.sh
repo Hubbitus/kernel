1713b6251cd98e29a29c670f0d8c257fa43d1062
Laura Abbott <labbott@fedoraproject.org>
2016-10-17 08:53:57 -0700

    Fixup rawhide RC script to actually work
    - Drop some debug output
    - Make fixup-bumpspec executable
    - copy kernel.spec.tmp back to kernel.spec
    - Typo in grep command
    - Increment base properly
    - Actually grab/upload tarball

--- scripts/rawhide-rc.sh
+++ scripts/rawhide-rc.sh
@@ -9,18 +9,28 @@ scripts/fixup-bumpspec.sh
 fedpkg commit -c
 
 # Figure out what is our RC
-RC=`grep kernel.spec "%define rcrev" | cut -d ' ' -f 3`
+RC=`grep "%define rcrev" kernel.spec| cut -d ' ' -f 3`
 RC=$(($RC+1))
+BASE=`grep "%define base_sublevel" kernel.spec| cut -d ' ' -f 3`
 
 # Kill all patches
 awk '!/patch/ { print $0 }' < sources > sources.tmp
 mv sources.tmp sources
 
 # Grab the tarball
-# FILL this in laura
+if [ ! -f patch-4.$BASE-rc$RC.xz ]; then
+	wget https://cdn.kernel.org/pub/linux/kernel/v4.x/testing/patch-4.$BASE-rc$RC.xz
+	if [ ! $? -eq 0 ]; then
+		exit 1
+	fi
+	fedpkg upload patch-4.$BASE-rc$RC.xz
+fi
 
 # bump rcrev in the spec and set git snapshot to 0
-
-perl -p -i -e 's|%define rcrev.*|%global rcrev $ENV{'RC'}|' kernel.spec
+RC=$RC perl -p -i -e 's|%define rcrev.*|%global rcrev $ENV{'RC'}|' kernel.spec
 
 perl -p -i -e 's|%define gitrev.*|%define gitrev 0|' kernel.spec
+
+perl -p -i -e 's|%global baserelease.*|%global baserelease 0|' kernel.spec
+
+rpmdev-bumpspec -c "Linux v4.$BASE-rc$RC" kernel.spec
