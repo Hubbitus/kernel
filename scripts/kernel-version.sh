0757590438b20f324d10068bf0aaea349754eb61
Laura Abbott <labbott@fedoraproject.org>
2017-02-23 15:09:44 -0800

    Update snapshot script for new sources format
    The sources format switched to sha512. Update the parsing to get the
    kernel version from this.

--- scripts/kernel-version.sh
+++ scripts/kernel-version.sh
@@ -1,8 +1,8 @@
-VER=$(grep patch sources | head -n1 | awk '{ print $2 }' | sed s/patch-// | sed s/-git.*// | sed s/.xz//)
+VER=$(grep patch sources | head -n1 | awk '{ print $2 }' | sed s/patch-// | sed s/-git.*// | sed s/.xz// | tr -d "()")
 
 if [ -z "$VER" ] ;
 then
-    VER=$(grep linux sources | head -1 | awk '{ print $2 }' | sed s/linux-// | sed s/.tar.xz//)
+    VER=$(grep linux sources | head -1 | awk '{ print $2 }' | sed s/linux-// | sed s/.tar.xz// | tr -d "()")
 fi
 
 
